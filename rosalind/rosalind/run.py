2e267063053a93fa4ba6bd1250f0b4cb5c710643
Jacob Pfeil <jpfei@ucsc.edu>
2018-06-22 12:00:31 -0700

    Updated run.py

--- rosalind/rosalind/run.py
+++ rosalind/rosalind/run.py
@@ -1,11 +1,45 @@
-#! /bin/usr/env python3
+#!/usr/bin/env python3.6
 import argparse
 
 
+def DNA(input, output):
+    """
+    Solves the first rosalind problem
+
+    :param input: Path to input file
+    :param output: Path to output file
+    :return:
+    """
+    pass
+
 
 def main():
-   pass
+    """
+    Runs the main routine of pipeline...
+    """
+    parser = argparse.ArgumentParser(description='Solve all of the rosalind problems...')
+    # Positional argument
+    parser.add_argument('problem',
+                        help='Rosalind problem ID')
+    parser.add_argument('-i', '--input',
+                        help='Path to input file',
+                        required=True)
+    parser.add_argument('-o', '--output',
+                        help='Path to output file',
+                        default="output.txt")
+    parser.add_argument('--debug',
+                        help='prints args',
+                        action='store_true')
+    args = parser.parse_args()
+
+    if args.debug:
+        print(args)
+
+    if args.problem == 'DNA':
+        DNA(args.input, args.output)
 
 
 if __name__ == '__main__':
     main()
+
+
